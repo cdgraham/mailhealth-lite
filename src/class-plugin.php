88f5f90ab573abf34083b4a2ecdd8ded9c9fc5d3
Christopher Graham <chris@cdgraham.com>
2025-08-13 23:02:07 -0400

    readme update

--- src/class-plugin.php
+++ src/class-plugin.php
@@ -18,7 +18,6 @@ class Plugin {
 	 * @return void
 	 */
 	public static function init( $main_file ): void {
-		load_plugin_textdomain( 'mailhealth-lite', false, dirname( plugin_basename( $main_file ) ) . '/languages' );
 		Admin\Menu::init();
 		Core\SmtpConfigurator::init();
 		Core\Logger::init();
